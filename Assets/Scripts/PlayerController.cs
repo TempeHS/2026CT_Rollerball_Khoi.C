85cd138567ad91109f3d49261b15eb8e9c6ab91e
KhoiC <khoi.cao1@education.nsw.gov.au>
2026-02-11 10:32:05 +1100

    WIP: Finished Score And UI
    - Added camera movement
    - Setup play area
    - Created collectables
    - Added collectable functionality
    - Created UI elements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
-
-
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -13,6 +16,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
     }
 
     void OnMove(InputValue movementValue)
@@ -23,10 +29,26 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
     }
 }
